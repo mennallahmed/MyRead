c8ef58db2580f1b5e91bc71df2dcf434efe9663a
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2021-12-23 16:30:22 +0200

    Update Shelves.js

--- src/Shelves.js
+++ src/Shelves.js
@@ -1,16 +1,16 @@
 import React from "react";
 import BookShelf from "./BookShelf";
 
-const Shelves = ({books}) => {
-  const currentlyReading = books.filter((book) => book.shelf === 'Currently Reading');
-  const wantToRead = books.filter((book) => book.shelf === 'Want to Read');
-  const read = books.filter((book) => book.shelf === 'Read');
+const Shelves = ({books ,changeShelf}) => {
+  const currentlyReading = books.filter((book) => book.shelf === 'currentlyReading');
+  const wantToRead = books.filter((book) => book.shelf === 'wantToRead');
+  const read = books.filter((book) => book.shelf === 'read');
 
   return (
     <div>
-      <BookShelf title ='Currently Reading' books ={currentlyReading}/>
-      <BookShelf title ='Want To Read' books={wantToRead}/>
-      <BookShelf title='Read' books={read}/>
+      <BookShelf title ='Currently Reading' books ={currentlyReading} changeShelf={changeShelf}/>
+      <BookShelf title ='Want To Read' books={wantToRead} changeShelf={changeShelf}/>
+      <BookShelf title='Read' books={read} changeShelf={changeShelf}/>
     </div>
   )
 
