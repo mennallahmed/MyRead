29acafb11b41346834c6e23f81c2a0d8deb6bcea
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2021-12-23 16:29:29 +0200

    Update Book.js

--- src/Book.js
+++ src/Book.js
@@ -1,19 +1,16 @@
 import React from "react";
+import ShelfChanger from "./ShelfChanger";
 
-const Book = ({bookDetails}) => {
+const Book = ({bookDetails, changeShelf}) => {
+
+
+ 
+  
   return(
     <div className="book">
             <div className="book-top">
               <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${bookDetails.url})` }}></div>
-              <div className="book-shelf-changer">
-                <select>
-                  <option value="move" disabled>Move to...</option>
-                  <option value="currentlyReading">Currently Reading</option>
-                  <option value="wantToRead">Want to Read</option>
-                  <option value="read">Read</option>
-                  <option value="none">None</option>
-                </select>
-              </div>
+              <ShelfChanger bookDetails={bookDetails} changeShelf={changeShelf}/>
             </div>
             <div className="book-title">{bookDetails.title}</div>
             <div className="book-authors">{bookDetails.author}</div>
