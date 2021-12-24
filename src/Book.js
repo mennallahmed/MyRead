65becb1d64414fae19cfaa789373711d0552d36e
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2021-12-24 18:17:15 +0200

    Update Book.js

--- src/Book.js
+++ src/Book.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ShelfChanger from "./ShelfChanger";
 
-const Book = ({bookDetails, changeShelf}) => {
+const Book = ({bookDetails, changeShelf, shelf}) => {
 
 
  
@@ -9,11 +9,11 @@ const Book = ({bookDetails, changeShelf}) => {
   return(
     <div className="book">
             <div className="book-top">
-              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${bookDetails.url})` }}></div>
-              <ShelfChanger bookDetails={bookDetails} changeShelf={changeShelf}/>
+              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${bookDetails.imageLinks.thumbnail})` }}></div>
+              <ShelfChanger bookDetails={bookDetails} changeShelf={changeShelf} shelf={shelf}/>
             </div>
             <div className="book-title">{bookDetails.title}</div>
-            <div className="book-authors">{bookDetails.author}</div>
+            <div className="book-authors">{bookDetails.authors && bookDetails.authors.join(', ')}</div>
           </div>
   )
  
