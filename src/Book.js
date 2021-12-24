6343157b7e2888629076357a8fd4603f13ef4cee
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2021-12-24 18:37:32 +0200

    Update Book.js

--- src/Book.js
+++ src/Book.js
@@ -3,16 +3,17 @@ import ShelfChanger from "./ShelfChanger";
 
 const Book = ({bookDetails, changeShelf, shelf}) => {
 
-
- 
+  const imageBackground =
+  bookDetails.imageLinks && bookDetails.imageLinks.thumbnail
+      ? bookDetails.imageLinks.thumbnail: "";
   
   return(
     <div className="book">
             <div className="book-top">
-              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${bookDetails.imageLinks.thumbnail})` }}></div>
+              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${imageBackground})` }}></div>
               <ShelfChanger bookDetails={bookDetails} changeShelf={changeShelf} shelf={shelf}/>
             </div>
-            <div className="book-title">{bookDetails.title}</div>
+            <div className="book-title">{bookDetails.title ? bookDetails.title : 'No title available'}</div>
             <div className="book-authors">{bookDetails.authors && bookDetails.authors.join(', ')}</div>
           </div>
   )
