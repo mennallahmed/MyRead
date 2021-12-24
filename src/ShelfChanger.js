d6725b1d7d1f26b06fd48601d4fa9673239a4295
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2021-12-24 18:18:15 +0200

    Update ShelfChanger.js

--- src/ShelfChanger.js
+++ src/ShelfChanger.js
@@ -1,15 +1,25 @@
 import React from "react";
 
 
-const ShelfChanger = ({bookDetails, changeShelf}) => {
+const ShelfChanger = ({bookDetails, changeShelf, shelf}) => {
 
   const onChangeShelf=(e)=>{
   	e.preventDefault()
     changeShelf(bookDetails, e.target.value)
   }
+  const defaultValue =() =>{
+    if(bookDetails.shelf){
+      return bookDetails.shelf
+    }
+    else if (shelf !=="none"){
+      return shelf
+    }
+    else return "none"
+  }
+
   return(
     <div className="book-shelf-changer">
-    <select defaultValue={bookDetails.shelf} onChange={onChangeShelf}>
+    <select defaultValue={defaultValue()} onChange={onChangeShelf}>
       <option value="move" disabled>Move to...</option>
       <option value="currentlyReading">Currently Reading</option>
       <option value="wantToRead">Want to Read</option>
